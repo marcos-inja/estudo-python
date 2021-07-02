26981df835a828410d0fc5c0bd7ab8fb67153429
marcos-inja <marcosvaas@gmail.com>
2021-07-02 10:23:20 -0300

    if_1 alterado

--- controle/if_1.py
+++ controle/if_1.py
@@ -1,11 +1,18 @@
 nota = float(input('Informe a nota do aluno 1: '))
+comportado = True if input('Comportado (s/n)') == 's' else False
 
 if nota >= 9:
+    if comportado:
+        print('Quadro de honra!')
+
     print('Para béns! kkk')
+
 elif nota >= 7:
     print('Passou')
+
 elif nota >= 4:
     print('Recuperação')
+    
 else:
     print('Sem jeito')
 
