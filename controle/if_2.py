84afe5cdb556e3fce6bbf74a3d8a10f8d5e3cf27
marcos-inja <marcosvaas@gmail.com>
2021-07-02 10:16:50 -0300

    if_2 add

---
+++ controle/if_2.py
@@ -0,0 +1,11 @@
+a = 'valor'  # True
+a = 0  # False
+a = ''  # False
+a = ' '  # True
+a = []  # False
+a = {}  # False
+
+if a:
+    print('Existe')
+else:
+    print('Não existe ou é zero ou vazio...')
